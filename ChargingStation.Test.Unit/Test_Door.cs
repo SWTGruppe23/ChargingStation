1fd8f5861f465917cc4a2fefd6896e5f2b81ad66
MarcusKluver1 <marcus.kluver@gmail.com>
2021-03-26 17:49:16 +0100

    Door tests

---
+++ ChargingStation.Test.Unit/Test_Door.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChargingStation.Door;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ChargingStation.Test.Unit
+{
+    public class Test_Door
+    {
+        private Door.Door _uut = new Door.Door();
+
+        [SetUp]
+        public void SetUp()
+        {
+            _uut = new Door.Door();
+        }
+
+        [Test]
+        public void IsDoorOpen()
+        {
+            //Arrange
+            EventHandler<DoorEventArgs> evtOpen = Substitute.For<EventHandler<DoorEventArgs>>();
+            _uut.DoorEvent += evtOpen;
+            //Act
+            _uut.OpenDoor();
+            //Assert
+            evtOpen.Received(1);
+        }
+
+        [Test]
+        public void IsDoorClosed()
+        {
+            //Arrange
+            EventHandler<DoorEventArgs> evtClose = Substitute.For<EventHandler<DoorEventArgs>>();
+            _uut.DoorEvent += evtClose;
+            //Act
+            _uut.CloseDoor();
+            //Assert
+            evtClose.Received(1);
+        }
+    }
+}

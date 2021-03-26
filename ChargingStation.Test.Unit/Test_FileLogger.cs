4961c775f5870800f07c9eff616fc1171b252985
andersKallesoee <anderskallesoee@gmail.com>
2021-03-26 18:35:40 +0100

    Tests for log

---
+++ ChargingStation.Test.Unit/Test_FileLogger.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChargingStation.Logger;
+using NUnit.Framework;
+
+namespace ChargingStation.Test.Unit
+{
+    class Test_FileLogger
+    {
+        private FileLogger _uut = new FileLogger();
+
+        [SetUp]
+        public void SetUp()
+        {
+        }
+
+        [Test]
+        public void Log()
+        {
+            _uut.log("test", 1);
+
+            System.IO.StreamReader file = new StreamReader(@"logfile.txt");
+            string line = file.ReadLine();
+
+            Assert.That(line.Contains("test"));
+        }
+    }
+}
